63a2ad66a30c2cc8b63d728a51c702a28e8f19aa
Shijin2k3 <shijinkumar2003@gmail.com>
2025-05-30 21:50:47 +0530

    handled error

--- sprint-1/src/App.jsx
+++ sprint-1/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
 import './App.css'
-import Navbar from './components/navbar/Navbar'
-import MobileNav from './components/navbar/MobileNav'
-import ResponsiveNav from './components/navbar/ResponsiveNav'
-import Hero from './components/hero/Hero'
 import About from './components/about/About'
-import Services from './components/services/Services'
 import Contact from './components/contact/Contact'
+import Hero from './components/hero/Hero'
+import ResponsiveNav from './components/navbar/ResponsiveNav'
+import Services from './components/services/Services'
+
 
 function App() {
 
